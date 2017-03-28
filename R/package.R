608a8548efa65c473990629fc03adb25da6ddf43
John Mount <jmount@win-vector.com>
2017-03-28 14:57:27 -0700

    spelling

--- R/package.R
+++ R/package.R
@@ -1,7 +1,7 @@
 #' \code{cdata}d: Wrappers for \code{tidyr::gather()} and \code{tidyr::spread()}
 #'
 #' cdata supplies deliberately verbose wrappers for \code{tidyr::gather()} and \code{tidyr::spread()} and an explanatory vignette.
-#' Useful for training and for enforcing pre-conditions.
+#' Useful for training and for enforcing preconditions.
 #'
 #' @docType package
 #' @name cdata
