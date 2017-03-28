6d4f0875829c86a6ec824815fe6d3926ab431026
John Mount <jmount@win-vector.com>
2017-03-28 15:16:48 -0700

    punctuation

--- R/package.R
+++ R/package.R
@@ -1,6 +1,6 @@
 #' \code{cdata}d: Wrappers for \code{tidyr::gather()} and \code{tidyr::spread()}
 #'
-#' cdata supplies deliberately verbose wrappers for \code{tidyr::gather()} and \code{tidyr::spread()} and an explanatory vignette.
+#' cdata supplies deliberately verbose wrappers for \code{tidyr::gather()} and \code{tidyr::spread()}, and an explanatory vignette.
 #' Useful for training and for enforcing preconditions.
 #'
 #' @docType package
