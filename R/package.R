f40d319b7609fb66968bb0ec299535ededea2c55
John Mount <jmount@win-vector.com>
2017-11-11 11:54:41 -0800

    re-build docs, start re-basing replyr on cdata 0.5.0

--- R/package.R
+++ R/package.R
@@ -1,6 +1,14 @@
-#' \code{cdata}: Wrappers for fluid data transform operators.
+#' \code{cdata}: Fluid Data Transformations.
 #'
-#' 
+#' Supplies implementations of higher order "fluid data" transforms.  These'
+#' transforms move data between rows and columns, are controlled by a graphical
+#' transformation specification, and have pivot and un-pivot as special cases.
+#' Implementation is based on 'DBI' and 'RSQLite', so should be usable on
+#' 'DBI' compliant data sources (include large systems such as 'PostgreSQL' and
+#' 'Spark').  Convenience adapters are provided for in-memory 'data.frame's.
+#' A theory of fluid data transforms can be found in the following articles:
+#' \url{https://github.com/WinVector/cdata} and \url{https://winvector.github.io/replyr/articles/FluidData.html}.
+#'
 #'
 #' @docType package
 #' @name cdata
