af0026a66939cffeb9ba63a8f52d71b458fe3f66
John Mount <jmount@win-vector.com>
2019-02-08 15:23:08 -0800

    extend test a bit

--- inst/unit_tests/test_composite_control_keys.R
+++ inst/unit_tests/test_composite_control_keys.R
@@ -3,8 +3,9 @@ test_composite_control_keys <- function() {
   # https://github.com/WinVector/cdata/issues/3
 
   d <- wrapr::build_frame(
-    "Sepal.Length", "Sepal.Width", "Petal.Length", "Petal.Width", "Species" |
-    5.1           , 3.5          , 1.4           , 0.2          , "setosa"  )
+    "Sepal.Length", "Sepal.Width", "Petal.Length", "Petal.Width", "Species", "id" |
+    5.1           , 3.5          , 1.4           , 0.2          , "setosa" , 1L   |
+    4.9           , 3            , 1.4           , 0.2          , "setosa" , 2L   )
 
   control_table <- wrapr::qchar_frame(
     Part,  Measure, Value        |
@@ -16,14 +17,18 @@ test_composite_control_keys <- function() {
   res <- rowrecs_to_blocks(d,
                            control_table,
                            controlTableKeys = c("Part", "Measure"),
-                           columnsToCopy = "Species")
+                           columnsToCopy = c("id", "Species"))
 
   expect <- wrapr::build_frame(
-    "Part", "Measure", "Value", "Species" |
-    "Sepal", "Length",  5.1,    "setosa"  |
-    "Sepal", "Width",   3.5,    "setosa"  |
-    "Petal", "Length",  1.4,    "setosa"  |
-    "Petal", "Width",   0.2,    "setosa"  )
+    "id", "Species", "Part" , "Measure", "Value" |
+    1L  , "setosa" , "Sepal", "Length" , 5.1     |
+    1L  , "setosa" , "Sepal", "Width"  , 3.5     |
+    1L  , "setosa" , "Petal", "Length" , 1.4     |
+    1L  , "setosa" , "Petal", "Width"  , 0.2     |
+    2L  , "setosa" , "Sepal", "Length" , 4.9     |
+    2L  , "setosa" , "Sepal", "Width"  , 3       |
+    2L  , "setosa" , "Petal", "Length" , 1.4     |
+    2L  , "setosa" , "Petal", "Width"  , 0.2     )
 
   RUnit::checkEquals(sort(colnames(expect)), sort(colnames(res)))
   res <- res[, colnames(expect), drop = FALSE]
@@ -33,7 +38,7 @@ test_composite_control_keys <- function() {
   RUnit::checkEquals(expect, res)
 
   back <- blocks_to_rowrecs(res,
-                            "Species",
+                            keyColumns = c("id", "Species"),
                             control_table,
                             controlTableKeys = c("Part", "Measure"))
 
